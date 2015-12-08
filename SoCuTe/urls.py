6a62367bde9c0a1323275b13bdae2f4753700217
anglov <anglov92@gmail.com>
2015-12-08 18:19:00 +0100

    Initial text

--- SoCuTe/urls.py
+++ SoCuTe/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from socute_app import views
 
 urlpatterns = [
     # Examples:
@@ -7,4 +8,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url('^index/?$', views.index),
+    url(r'^/?$', views.index),
 ]
--- socute_app/views.py
+++ socute_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+from socute_app.forms import TextForm
+from socute_app.models import *
+
+def index(request):
+    if request.method == 'POST': # Jeśli zapytanie jest post to
+        # próbujemy przetworzyć dane z od użytkownika
+        form = TextForm(request.POST) # Tworzymy formularz
+        if form.is_valid(): # Jeśli jest poprawny
+            text = TextModel()
+            text.text = request.POST['text']
+            text.full_clean()
+            text.save()
+    elif request.method == 'GET':
+        form = TextForm() # Zapytanie jest GET więc tworzymy formularz
+    else:
+        return HttpResponse(status=403)
+    # Tutaj możemy dość jeśli:
+    # * Zapytanie jest GET (wtedy tworzymy nowy formularz)
+    # * Zapytanie jest POST i formularz jest **niepoprawny**,
+    #   wtedy pojawia się formularz z zaznaczonymi blędami
+    return render(request, 'socute_app/index.html', {'form': form})
